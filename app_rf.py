10a31698872ca0d0caff8af27a1f61cdf02e2a2e
julio Paredes <111199760+Japr2206@users.noreply.github.com>
2025-04-04 12:27:19 -0300

    Update app_rf.py

--- app_rf.py
+++ app_rf.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1aKj4ermYXsIktppxsBqElfKUURiJ9Ek8
 """
 
-pip install streamlit
+#pip install streamlit
 
 import streamlit as st
 import pandas as pd
